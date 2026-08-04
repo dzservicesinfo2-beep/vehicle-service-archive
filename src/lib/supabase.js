f0e1554fb9c15a81086325014463c595f6fcb351
Dainis Zeile <dzservicesinfo2@gmail.com>
2026-08-04 15:28:05 +0100

    Sanitize Supabase project URL

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const rawSupabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL
+
+const supabaseKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+
+const supabaseUrl = new URL(rawSupabaseUrl).origin
 
 export const supabase = createClient(
   supabaseUrl,
